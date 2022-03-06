ade046b0b9af3c0c04f50a7f332bd96da163d1ef
George Lemmon <george.lemmon@gmail.com>
2022-03-06 11:36:37 -0700

    Add traveler testing

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,8 +1,27 @@
 import chai from 'chai';
+import Traveler from '../src/Traveler';
 const expect = chai.expect;
 
-describe('See if the tests are running', function () {
-  it('should return true', function () {
-    expect(true).to.equal(true);
+describe('Traveler', () => {
+  const traveler = new Traveler(5, 'Conan the Barbarian', 'Pillager');
+
+  it('should be a function', function () {
+    expect(Traveler).to.be.a('function');
   });
+
+  it('should be an instance of Traveler', function () {
+    expect(traveler).to.be.an.instanceOf(Traveler);
+  });
+
+  it('should have an ID', function () {
+    expect(traveler.id).to.equal(5)
+  })
+
+  it('should have a name', function () {
+    expect(traveler.fullName).to.equal('Conan the Barbarian')
+  })
+
+  it('should have a travaler type', function () {
+    expect(traveler.type).to.equal('Pillager')
+  })
 });
